0af7c53377d2fd48be39894cce86ab242747b6ff
Andrei Mihai Adrian <mihai.andrei1597@gmail.com>
2023-07-27 22:21:21 +0300

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import NinjaImage from "./images/ninja.png";
 
 const App: Component = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class="h-full flex flex-col justify-center items-center gap-5">
+      <img src={NinjaImage} />
+      <h1 class="text-3xl text-white">Quiz App</h1>
+      <h1 class="text-3xl text-white">Test your CSS knowledge</h1>
+      <p class="text-gray-400 max-w-xl text-center">
+        Test your CSS knowledge with our quiz app and see if you're a true web
+        design whiz!
+      </p>
+      <button class="bg-blue-400 px-8 py-4 rounded-2xl text-white">
+        Start
+      </button>
+    </div>
   );
 };
 
